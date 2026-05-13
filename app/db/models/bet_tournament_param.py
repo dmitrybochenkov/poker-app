0b0571e2c2de842f0dd07eba6dfa81a0c5dc75a4
Dmitry Bochenkov <pishisudapridurok@yandex.ru>
2026-05-13 17:39:04 +0300

    Some fixes for Bet

--- app/db/models/bet_tournament_param.py
+++ app/db/models/bet_tournament_param.py
@@ -7,12 +7,12 @@ from app.db.base import Base
 class BetTournamentParam(Base):
   __tablename__ = "bet_tournament_params"
   __table_args__ = (
-    UniqueConstraint("type", name="uq_bet_tournament_params_type"),
+    UniqueConstraint("tournament_type", name="uq_bet_tournament_params_type"),
   )
 
   row_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-  tournament_type: Mapped[str] = mapped_column("type", String(16), nullable=False)
-  bet_param_id: Mapped[int] = mapped_column("bet_params_id", Integer, nullable=False)
+  tournament_type: Mapped[str] = mapped_column(String(16), nullable=False)
+  bet_param_id: Mapped[int] = mapped_column(Integer, nullable=False)
   percent_to_first: Mapped[int] = mapped_column(Integer, nullable=False, default=50)
   percent_to_second: Mapped[int] = mapped_column(Integer, nullable=False, default=30)
   percent_to_third: Mapped[int] = mapped_column(Integer, nullable=False, default=20)
