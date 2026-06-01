6ab653ec9e5c6b6357be12012091853c2d23fd31
Dmitry Bochenkov <pishisudapridurok@yandex.ru>
2026-06-01 21:21:17 +0300

    Webapp background

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="shell">
     <div class="table-bg" aria-hidden="true">
-      <div class="seat seat-1"></div>
-      <div class="seat seat-2"></div>
-      <div class="seat seat-3"></div>
-      <div class="seat seat-4"></div>
-      <div class="seat seat-5"></div>
-      <div class="seat seat-6"></div>
-      <div class="seat seat-7"></div>
-      <div class="seat seat-8"></div>
-      <div class="seat seat-9"></div>
-      <div class="seat seat-10"></div>
-      <div class="board">
-        <span class="card-slot"></span>
-        <span class="card-slot"></span>
-        <span class="card-slot"></span>
-        <span class="card-slot"></span>
-        <span class="card-slot"></span>
+      <div class="table-rail">
+        <div class="table-felt">
+          <div class="seat seat-1"></div>
+          <div class="seat seat-2"></div>
+          <div class="seat seat-3"></div>
+          <div class="seat seat-4"></div>
+          <div class="seat seat-5"></div>
+          <div class="seat seat-6"></div>
+          <div class="seat seat-7"></div>
+          <div class="seat seat-8"></div>
+          <div class="seat seat-9"></div>
+          <div class="board">
+            <span class="card-slot"></span>
+            <span class="card-slot"></span>
+            <span class="card-slot"></span>
+            <span class="card-slot"></span>
+            <span class="card-slot"></span>
+          </div>
+        </div>
       </div>
     </div>
     <header class="topbar">
