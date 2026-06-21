f17eb69423e4ac2290dd7c5c6af95ea3513b804e
Dmitry Bochenkov <pishisudapridurok@yandex.ru>
2026-06-22 02:12:56 +0300

    TG app fixes

--- scripts/local_deploy.sh
+++ scripts/local_deploy.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 SERVER_HOST="${1:-poker-vds}"
+SERVER_PORT="${SERVER_PORT:-999}"
 REMOTE_ROOT="${2:-/opt/poker-app}"
 REMOTE_SCRIPT="${REMOTE_ROOT}/scripts/server_full_update.sh"
 
-ssh "$SERVER_HOST" "cd '$REMOTE_ROOT' && '$REMOTE_SCRIPT'"
+ssh -p "$SERVER_PORT" "$SERVER_HOST" "cd '$REMOTE_ROOT' && '$REMOTE_SCRIPT'"
